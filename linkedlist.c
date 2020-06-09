95236ec244bad765584ad775b2dfbc7be933627e
Aditya <sangamnerkaram@rknec.edu>
2020-06-09 17:12:16 +0530

    completed

--- linkedlist.c
+++ linkedlist.c
@@ -7,65 +7,51 @@ struct Node
 	struct Node* next;
 
 };
-
-
-
-
-void print(struct Node* head)
+struct Node* head = NULL;
+void print()
 {
-	printf("\nLinked List: ");
-	while(head!=NULL)
+	printf("\nLINKED LIST: ");
+	struct Node* temp = head;
+	if(temp == NULL)
+		printf("Linked List is empty....");
+	else
 	{	
-		if(head->next == NULL)
-			printf("%d->NULL",head->data);
+	while(temp!=NULL)
+	{	
+		if(temp->next == NULL)
+			printf("%d-->NULL",temp->data);
 		else
-			printf("%d->",head->data);
-		head = head->next;	
+			printf("%d-->",temp->data);
+		temp = temp->next;	
+	}
 	}
 }
-struct Node* insert_beg( struct Node* head,int x)
+void append(int x)
 {
-	struct Node* temp;
-	temp = (struct Node*)malloc(sizeof(struct Node));
-	temp->data = x;
-	temp->next = NULL;
-
+	struct Node* temp1;
+	temp1 = (struct Node*)malloc(sizeof(struct Node));
+	temp1->data = x;
+	temp1->next = NULL;
+	struct Node* temp2 = head;
 	if(head == NULL)
 	{
-		head = temp;	
+		head = temp1;
 	}
 	else
 	{
-		temp->next = head;
-		head = temp;
-	}
-
-	return head;
-}
-
-void insert_end(struct Node* head, int x)
-{
-	struct Node* temp;
-	temp = (struct Node*)malloc(sizeof(struct Node));
-	temp->data = x;
-	temp->next = NULL;
-	if(head == NULL)
-		printf("Empty List..insert at the beginning first");
-	else
-	{
-		while(head!=NULL)
+		while(temp2!=NULL)
 		{
-			if(head->next == NULL)
+			if(temp2->next == NULL)
 			{
-				head->next = temp;
+				temp2->next = temp1;
 				break;			
 			}
-		head = head->next;
+		temp2 = temp2->next;
 		}
 		
 	}
 }
-struct Node* insert_n(int x,int  pos,struct Node* head)
+void insert_n(int x,int  pos)
 {
 	struct Node*  temp1;
 	int i;
@@ -78,13 +64,14 @@ struct Node* insert_n(int x,int  pos,struct Node* head)
 	{
 		temp1->next = head;
 		head = temp1;	
+		printf("Node inserted successfully...");
 	}
 	else
 	{
 		if(length(head)<pos)
 		{
-			printf("Index invalid ....");
-			return head;
+			printf("Index invalid....");
+			free(temp1);
 					
 		}
 		else
@@ -95,23 +82,97 @@ struct Node* insert_n(int x,int  pos,struct Node* head)
 			}
 			temp1->next = temp2->next;
 			temp2->next = temp1;
+			printf("Node inserted successfully...");
 					
 		}
 	}
-	printf("Insertion successful at index %d ....",pos);
-	return head;
+	
 
 	
 }
-int length(struct Node* head)
+int isEmpty()
+{
+	if(head == NULL)
+		return 0;
+	else
+		return 1;
+}
+int length()
 {
 	int length = 0;
-	while(head!=NULL)
+	struct Node* temp = head;	
+	while(temp!=NULL)
 	{
 		length++;
-		head = head->next;	
+		temp = temp->next;	
 	}
 	return length;
+}
+void  delete_n(int pos)
+{
+	int i,len;
+	len = length(head);	
+	struct Node* temp1 = head;
+	if(isEmpty() == 0)
+		printf("Linked List already empty :p...");
+	else if(pos == 1)
+	{
+		head = temp1->next;
+		free(temp1);	
+		
+	}
+	else if(pos > len)
+	{
+		printf("Index invalid...");
+			
+	}
+	else
+	{
+	
+	for(i=0;i<pos-2;i++)
+		temp1 = temp1->next;
+	struct Node* temp2 = temp1->next;
+	temp1->next = temp2->next;
+	free(temp2);
+	printf("Node deleted sucessfully ...");
+	}
+	
+}
+void modify(int x,int pos)
+{
+	struct Node* temp1 = head;
+	int i;
+	if(length(head)<pos)
+		printf("Index invalid");
+	else
+	{
+		for(i=0;i<pos;i++)
+		{
+			temp1 = temp1->next;		
+		}	
+		temp1->data = x;
+		printf("Node modified successfully...");
+	}
+}
+void reverse()
+{
+	struct Node *current,*prev,*next;
+	current = head;
+	prev = NULL;
+	if(length(head)<2)
+		printf("Cannot be reversed...");
+	else
+	{
+	while(current!=NULL)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;	
+	}
+	head = prev;
+	}
+
 }
 void main()
 {
@@ -124,46 +185,67 @@ void main()
 	{
 		system("clear");		
 		printf(".........MENU.........");
-		printf("\n1.Insert node to beginning of Linked list");
-		printf("\n2.Display the linked list");
-		printf("\n3.Insert node at the end of Linked list(Append)");
-		printf("\n4.Length of Linked list");
-		printf("\n5.Insert node at nth index");
+		printf("\n1.Append");
+		printf("\n2.Display");
+		printf("\n3.Insert @nth position");
+		printf("\n4.Length");
+		printf("\n5.Delete @nth position");
+		printf("\n6.Modify");
+		printf("\n7.Reverse");
+		printf("\n8.Exit");
 		printf("\nEnter your choice : ");
 		scanf("%d",&ch);
 		switch(ch)
 		{
 			case 1:
-				printf("Enter the data : ");
+				printf("\nEnter the data : ");
 				scanf("%d",&x);
-				head = insert_beg(head , x);
-				printf("Node added successfully...");
+				append(x);
 				break;	
 			case 2:
-				print(head);
-				break;	
+				print();
+				break;
+			
 			case 3:
-				printf("Enter the data : ");
-				scanf("%d",&x);
-				insert_end(head,x);
-				printf("Node added successfully...");
-				break;
-			case 4:
-				printf("Length of linked list is %d",length(head));
-				break;
-			case 5:
-				print(head);
+				print();
 				printf("\nEnter the index where you want to insert data : ");
 				scanf("%d",&n);
 				printf("\nEnter the data : ");
 				scanf("%d",&x);
-				head = insert_n(x,n,head);
+				insert_n(x,n);
+				break;
+			case 4:
+				printf("\nLength of linked list is %d",length(head));
+				break;
+
+			case 5:
+				print(head);
+				printf("\nEnter the index of node you want to delete : ");
+				scanf("%d",&n);
+				delete_n(n);
+				break;
+			case 6:
+				print();
+				printf("\nEnter the index of node you want to modify : ");
+				scanf("%d",&n);
+				printf("\nEnter the modified data : ");
+				scanf("%d",&x);
+				modify(x,n);
+				break;
+			case 7:
+				print();				
+				reverse();
+				print();
+				break;
+			case 8:
+				
+				exit(0);
 				break;
 		
 			default: 
-				printf("Invalid Choice!!!");
+				printf("\nInvalid Choice!!!");
 		}
-		printf("\nDo you wish too continue 1/0");
+		printf("\nDo you wish too continue 1/0 : ");
 		scanf("%d",&cont);
 	}
 	while(cont==1);	
